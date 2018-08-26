ccdb1e7b65912b04da79acc6d6431cd9f486edf7
John Wyles <john@fourstacks.co.uk>
2018-08-26 21:02:00 +0100

    Add detail field view

--- resources/js/components/DetailField.vue
+++ resources/js/components/DetailField.vue
@@ -1,9 +1,33 @@
 <template>
-    <panel-item :field="field" />
+    <panel-item :field="field">
+        <ul slot="value" class="list-reset">
+            <component
+                v-for="option in optionList"
+                :key="option.key"
+                :is="getItemType(option)"
+                :option="option"
+            ></component>
+        </ul>
+    </panel-item>
 </template>
 
 <script>
-export default {
-    props: ['resource', 'resourceName', 'resourceId', 'field'],
-}
+    import CheckboxDisplay from '../mixins/CheckboxDisplay'
+
+    export default {
+
+        mixins: [ CheckboxDisplay ],
+
+        props: ['resource', 'resourceName', 'resourceId', 'field'],
+
+        computed: {
+            optionList() {
+                if (this.field.display_unchecked_on_detail) {
+                    return this.getAllOptions();
+                }
+                return this.getCheckedOptions();
+            }
+        }
+
+    }
 </script>
